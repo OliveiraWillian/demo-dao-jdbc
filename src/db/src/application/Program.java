922b44afa84e26d710a39a77098f2514bb5843fd
Willian Oliviera <willi12345677@gmail.com>
2025-08-31 13:06:46 -0300

    OK

---
+++ src/db/src/application/Program.java
@@ -0,0 +1,12 @@
+package db.src.application;
+
+import db.src.db.DB;
+
+import java.sql.Connection;
+
+public class Program {
+    public static void main(String[] args) {
+        Connection conn = DB.getConnection();
+        DB.closeConnection();
+    }
+}
